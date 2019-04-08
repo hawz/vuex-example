803453863cc0d4f04cc8e40e4cabcac5a152a58a
Alberto Masia <alberto.masia@gmail.com>
2019-04-08 11:27:08 +0200

    Add counter components and result components.

---
+++ src/components/Result.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p>First component Counter is: {{ counter }}</p>
+    <p>First component Clicks is: {{ clicks }}</p>
+  </div>
+</template>
+
+<script>
+import * as types from "../store/types";
+export default {
+  computed: {
+    counter() {
+      return this.$store.getters[types.DOUBLE_COUNTER];
+    },
+    clicks() {
+      return this.$store.getters[types.CLICK_COUNTER];
+    }
+  }
+};
+</script>
